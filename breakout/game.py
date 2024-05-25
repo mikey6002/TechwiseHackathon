d3214439efd676a1735f11542de5af99f3ae8ab8
pistachionet <mynavid@verizon.net>
2024-05-24 21:53:34 -0400

    paddle

--- breakout/game.py
+++ breakout/game.py
@@ -3,6 +3,8 @@ import pygame
 from pygame.locals import *
 import time
 
+pygame.init()
+
 class Game:
     def __init__(self) -> None:
         pygame.init()
@@ -22,6 +24,28 @@ class Game:
             pygame.display.flip()
             time.sleep(SLEEP_TIME)
         pygame.quit()
+
+
+clocl = pygame.time.Clock()
+
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+class Paddle: 
+    def __init__(self):
+        self.width = 100
+        self.height = 20
+        self.x = (screen_width - self.width) // 2
+        self.y = (screen_height - self.height) - 30
+        self.speed = 5
+    def draw(self):
+        pygame.draw.rect(screen, GREEN, (self.x, self.y, self.width, self.height))
+    
+    def move(self, direction):
+        if direction == "left" and self.x 
 
 if __name__ == "__main__":
     game = Game()
