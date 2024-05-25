b8115e9ad7f1cf1679ba7bbf889e8a52e8a9b3b5
pistachionet <mynavid@verizon.net>
2024-05-24 21:32:13 -0400

    start

--- breakout/game.py
+++ breakout/game.py
@@ -7,16 +7,18 @@ class Game:
     def __init__(self) -> None:
         pygame.init()
         self.running = True
-        pygame.display.set_caption("Boids")
+        pygame.display.set_caption("Breakout")
         self.surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
         self.surface.fill(WHITE)
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == pygame.QUIT:
                     self.running = False
-            pygame.draw.circle(self.surface, BLACK, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), BOID_RADIUS)
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        print("the up key has been pressed down")
             pygame.display.flip()
             time.sleep(SLEEP_TIME)
         pygame.quit()
